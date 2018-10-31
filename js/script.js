e7a99eb925427018d5e4c46f01b5eac2c80f3ff0
Edite Fernandes <editeccfernandes@gmail.com>
2018-10-31 11:26:31 +0000

    consolidação da info nos primeiros 3 slides

--- js/script.js
+++ js/script.js
@@ -1,33 +1,53 @@
-window.onload = function(){
-    console.log('Documento carregado');
-//quando o documento carregar todo, correr estes blocos de script
-//Intro,som e fades
-$('.intro-img').click(function () {
-    //toca áudio no click
-    $('#audio-intro').trigger('play');
-    $('.intro-container').fadeOut( 3000, 'swing');
-    //"deblur de cenas"
-    $('.blur').css({ "filter": "blur(0px)"});
-    $('.slide-txt').delay(3000).animate({'opacity':'1'}, 3000);
-});
+window.onload = function () {
+	console.log('Documento carregado');
+	//quando o documento carregar todo, correr estes blocos de script
+	//Intro,som e fades
+	$('.intro-img').click(function () {
+		//toca áudio no click
+		$('#audio-intro').trigger('play');
+		$('.intro-container').fadeOut(3000, 'swing');
+		//"deblur de cenas"
+		$('.blur').css({ "filter": "blur(0px)" });
+		$('.slide-txt').delay(3000).animate({ 'opacity': '1' }, 3000);
+	});
+
+	$('.slide a').click(function(e){
+        e.preventDefault();
+        var target = $(this).attr('href');
+        var distance_top = $(target).offset().top;
+        $('html, body').animate({scrollTop:distance_top},1000);
+    });
+
+	//Parallax is this ai
+	$('#isthisai').mousemove(function (parallax) {
+		var window_width = $(window).width();
+		var margin1 = -(parallax.pageX - (window_width / 2)) / 2;
+		var margin2 = -(parallax.pageX - (window_width / 2)) / 5;
+		var margin3 = -(parallax.pageX - (window_width / 2)) / 10;
+
+		$('#isthisai_parallax_1').css({ 'margin-left': margin1 });
+		$('#isthisai_parallax_2').css({ 'margin-left': margin2 });
+		$('#isthisai_parallax_3').css({ 'margin-left': margin3 });
+	});
 
 
-//timeline
-var timeline_block = $('.timeline-block');
+
+	//timeline
+	var timeline_block = $('.timeline-block');
 	//blocos de texto da timeline fora do viewport estão escondidos
-	timeline_block.each(function(){
-		if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
+	timeline_block.each(function () {
+		if ($(this).offset().top > $(window).scrollTop() + $(window).height()) {
 			$(this).find('.timeline-img, .timeline-content').addClass('timeline-block-hide');
 		}
-    });
-    
-    //quando entram dentro do viewport são animados
-	$(window).on('scroll', function(){
-		timeline_block.each(function(){
-			if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.timeline-img').hasClass('timeline-block-hide') ) {
+	});
+
+	//quando entram dentro do viewport são animados a partir do centro do ecrã
+	$(window).on('scroll', function () {
+		timeline_block.each(function () {
+			if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * 0.75 && $(this).find('.timeline-img').hasClass('timeline-block-hide')) {
 				$(this).find('.timeline-img, .timeline-content').removeClass('timeline-block-hide').addClass('timeline-block-show');
 			}
 		});
-    });
-    
+	});
+
 };
