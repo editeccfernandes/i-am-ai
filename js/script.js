cc19efa0d618f32f165d0d305b772e90580bf4aa
editeccfernandes <dangerouswho@gmail.com>
2018-11-01 21:26:38 +0000

    Consolidação do trabalho até à timeline

--- js/script.js
+++ js/script.js
@@ -1,25 +1,36 @@
 window.onload = function () {
 	console.log('Documento carregado');
-	$('.intro-img').css({backgroundColor:'#00ff4071'});
+	$('.intro-img').css({ backgroundColor: '#00ff4071' });
 	//quando o documento carregar todo, correr estes blocos de script
 	//Intro,som e fades
+	$('#intro-video').prop("volume", 0.0);
+	$('#intro-video').prop("volume", 0.0);
+
 	$('.intro-img').click(function () {
-		$('.intro-img').css({backgroundColor:'#ffffff'});
-		$('.intro-img svg').css({filter: 'invert(100%)'})
+		$('.intro-img').css({ backgroundColor: '#ffffff' });
+		$('.intro-img svg').css({ filter: 'invert(100%)' })
 		//toca vídeo no click
-		$('#intro-video').trigger('play');
+		$('#intro-video').trigger('play').prop('muted', false).animate({ volume: 1 }, 1000);
 		$('.intro-container').fadeOut(3000, 'swing');
 		//"deblur de cenas"
 		$('.blur').css({ "filter": "blur(0px)" });
 		$('.slide-txt').delay(3000).animate({ 'opacity': '1' }, 3000);
 	});
 
-	$('.slide a').click(function(e){
-        e.preventDefault();
-        var target = $(this).attr('href');
-        var distance_top = $(target).offset().top;
-        $('html, body').animate({scrollTop:distance_top},1000);
-    });
+	$('.button-yes').click(function () {
+		$('#intro-video').animate({ volume: 0 }, 1000);
+		//$('#common-humanity').trigger('play').animate({volume: 0.3}, 1000);
+	});
+
+
+
+
+	$('.slide a').click(function (e) {
+		e.preventDefault();
+		var target = $(this).attr('href');
+		var distance_top = $(target).offset().top;
+		$('html, body').animate({ scrollTop: distance_top }, 1000);
+	});
 
 	//Parallax is this ai
 	$('#isthisai').mousemove(function (parallax) {
@@ -36,20 +47,35 @@ window.onload = function () {
 	});
 
 
+	//scroll dentro da div da timeline
+
+
+	  $( "#descer" ).click(function(){
+		$('#brief-history').animate({scrollTop: $('#brief-history').scrollTop()+600}, 1000, 'swing');
+	});
+
+	$( "#subir" ).click(function(){
+		$('#brief-history').stop().animate({scrollTop: $('#brief-history').scrollTop()-600}, 1000, 'swing');
+	});
+
+	
+	//if( ($('#brief-history').scrollTop() <= 0)) {
+	//	console.log('adeus')
+	//};
 
 	//timeline
 	var timeline_block = $('.timeline-block');
 	//blocos de texto da timeline fora do viewport estão escondidos
 	timeline_block.each(function () {
-		if ($(this).offset().top > $(window).scrollTop() + $(window).height()) {
+		if ($(this).offset().top > $('#brief-history').scrollTop() + $('#brief-history').height()) {
 			$(this).find('.timeline-img, .timeline-content').addClass('timeline-block-hide');
 		}
 	});
 
-	//quando entram dentro do viewport são animados a partir do centro do ecrã
-	$(window).on('scroll', function () {
+	//quando "entram" dentro da div são animados chegaram e apareceram
+	$('#brief-history').on('scroll', function () {
 		timeline_block.each(function () {
-			if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * 0.75 && $(this).find('.timeline-img').hasClass('timeline-block-hide')) {
+			if ($(this).position().top <= $('#brief-history').scrollTop() + $('#brief-history').height() * 0.3  && $(this).find('.timeline-img').hasClass('timeline-block-hide')) {
 				$(this).find('.timeline-img, .timeline-content').removeClass('timeline-block-hide').addClass('timeline-block-show');
 			}
 		});
